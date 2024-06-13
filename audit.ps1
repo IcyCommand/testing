993e844678773d291026106936bd9029e11a4c12
IcyCommand <112899933+IcyCommand@users.noreply.github.com>
2024-06-13 14:08:20 +0100

    Update audit.ps1

--- audit.ps1
+++ audit.ps1
@@ -1,1 +1,7 @@
-Get-ADObject 'CN=ms-mcs-admpwd,CN=Schema,CN=Configuration,DC=testad,DC=local'
+# Prompt for the local admin credentials
+$adminUsername = "Administrator"
+$securePassword = Read-Host -Prompt "Enter password for $adminUsername" -AsSecureString
+$credential = New-Object System.Management.Automation.PSCredential($adminUsername, $securePassword)
+
+# Start a new PowerShell process with the provided admin credentials
+Start-Process powershell.exe -Credential $credential -ArgumentList '-NoExit -Command "Start-Process PowerShell -Verb RunAs"'
